aa76cecb7b15f113e587e66d033ed90a5801b006
Luyang Xing <leon.luyangxing@gmail.com>
2013-05-20 14:45:24 +0800

    Ver.0.32a
    debug

--- autore/views.py
+++ autore/views.py
@@ -107,22 +107,24 @@ def response_msg(request):
 
             try:
                 kwlobjs = KeywordsList.objects.filter(cKeywords=content)
-                for kwlobj in kwlobjs:
-                    if kwlobj.cMsgType == 'text':
-                        return HttpResponse(reply_text % (toUserName, fromUserName, postTime, kwlobj.cMsgType,
-                                                          kwlobj.cContent))
-                    elif kwlobj.cMsgType == 'news':
-                        return HttpResponse(reply_news % (toUserName, fromUserName, postTime, kwlobj.cMsgType,
-                                                          kwlobj.cArticleCount,
-                                                          kwlobj.cTitle1, kwlobj.cDescription1, kwlobj.cPicUrl1, kwlobj.cUrl1,
-                                                          kwlobj.cTitle2, kwlobj.cDescription2, kwlobj.cPicUrl2, kwlobj.cUrl2,
-                                                          kwlobj.cTitle3, kwlobj.cDescription3, kwlobj.cPicUrl3, kwlobj.cUrl3,
-                                                          kwlobj.cTitle4, kwlobj.cDescription4, kwlobj.cPicUrl4, kwlobj.cUrl4))
-                    elif kwlobj.cMsgType == 'music':
-                        return HttpResponse(reply_music % (toUserName, fromUserName, postTime, kwlobj.cMsgType,
-                                                           kwlobj.cTitle, kwlobj.cDescription, kwlobj.cMusicUrl, kwlobj.cHQMusicUrl))
-                    else:
-                        return HttpResponse("Invalid Request")
+                test = "%s,%s,%s,%s" % (kwlobjs[0].cKeywords, kwlobjs[0].cMsgType, kwlobjs[0].cContent, kwlobjs[0])
+                return HttpResponse(reply_text % (toUserName, fromUserName, postTime, 'text', test))
+                # for kwlobj in kwlobjs:
+                #     if kwlobj.cMsgType == 'text':
+                #         return HttpResponse(reply_text % (toUserName, fromUserName, postTime, kwlobj.cMsgType,
+                #                                           kwlobj.cContent))
+                #     elif kwlobj.cMsgType == 'news':
+                #         return HttpResponse(reply_news % (toUserName, fromUserName, postTime, kwlobj.cMsgType,
+                #                                           kwlobj.cArticleCount,
+                #                                           kwlobj.cTitle1, kwlobj.cDescription1, kwlobj.cPicUrl1, kwlobj.cUrl1,
+                #                                           kwlobj.cTitle2, kwlobj.cDescription2, kwlobj.cPicUrl2, kwlobj.cUrl2,
+                #                                           kwlobj.cTitle3, kwlobj.cDescription3, kwlobj.cPicUrl3, kwlobj.cUrl3,
+                #                                           kwlobj.cTitle4, kwlobj.cDescription4, kwlobj.cPicUrl4, kwlobj.cUrl4))
+                #     elif kwlobj.cMsgType == 'music':
+                #         return HttpResponse(reply_music % (toUserName, fromUserName, postTime, kwlobj.cMsgType,
+                #                                            kwlobj.cTitle, kwlobj.cDescription, kwlobj.cMusicUrl, kwlobj.cHQMusicUrl))
+                #     else:
+                #         return HttpResponse("Invalid Request")
             except KeywordsList.DoesNotExist:
                 pass
 
